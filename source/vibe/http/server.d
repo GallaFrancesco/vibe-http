5b40228c0e1e83ea4e7275de80f74dc27f4b89a7
GallaFrancesco <francesco.galla3@gmail.com>
2018-12-07 23:26:39 +0100

    Modified writeheader to accept a destination stream

--- source/vibe/http/server.d
+++ source/vibe/http/server.d
@@ -1490,7 +1490,7 @@ unittest {
 
 	//void write(alias HeaderCallback, alias BodyCallback)()
 	//{
-		//connection.writeHeaders!HeaderCallback();
+		//connection.writeHeader!HeaderCallback();
 		//connection.writeBody!BodyCallback();
 	//}
 //}
@@ -1539,7 +1539,7 @@ struct HTTPServerRequestData {
 			if (!m_peer) {
 				version (Have_vibe_core) {} else scope (failure) assert(false);
 				// store the IP address (IPv4 addresses forwarded over IPv6 are stored in IPv4 format)
-				auto peer_address_string = this.clientAddress.toString();
+				auto peer_address_string = this.clientAddress.toAddressString();
 				if (peer_address_string.startsWith("::ffff:") && peer_address_string[7 .. $].indexOf(':') < 0)
 					m_peer = peer_address_string[7 .. $];
 				else m_peer = peer_address_string;
@@ -1575,6 +1575,11 @@ struct HTTPServerRequestData {
 				_path = urlDecode(requestPath.toString);
 			}
 			return _path.get;
+		}
+
+		void path(string st) @safe {
+			assert(_path.isNull, "Unable to set request path");
+			_path = st;
 		}
 
 		private Nullable!string _path;
@@ -1903,6 +1908,7 @@ struct HTTPServerResponseData {
 	}
 
 	protected {
+
 		/// The protocol version of the response - should not be changed
 		HTTPVersion httpVersion = HTTPVersion.HTTP_1_1;
 
@@ -2489,12 +2495,19 @@ struct HTTPServerResponseData {
 	}
 
 	private void writeHeader()
-		@safe {
+	@safe {
+		writeHeader(m_conn);
+	}
+
+	// accept a destination stream
+	private void writeHeader(Stream)(Stream conn) @safe
+		if(isStream!Stream || isOutputStream!Stream)
+	{
 			import vibe.stream.wrapper;
 
 			assert(!m_bodyWriter && !m_headerWritten, "Try to write header after body has already begun.");
 			m_headerWritten = true;
-			auto dst = streamOutputRange!1024(m_conn);
+			auto dst = streamOutputRange!1024(conn);
 
 			void writeLine(T...)(string fmt, T args)
 				@safe {
